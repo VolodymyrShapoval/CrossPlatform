a413c33079076a86692536c459134bd6c5f9208b
VolodymyrS <volodymyr.sh.05@gmail.com>
2024-09-27 00:12:44 +0300

    added method for reading .txt files, added xUnit project

---
+++ Lab1.Source/Methods/HeadsAndTails.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.Source.Methods
+{
+    internal class HeadsAndTails
+    {
+        public HeadsAndTails() { }
+
+        /// <summary>
+        /// Method for calculating the number of sequences of coin toss results
+        /// </summary>
+        /// <param name="tossesNum">Number of tosses</param>
+        /// <param name="tailsNum">The number of times the "tail" fell out</param>
+        /// <returns></returns>
+        public static int CalculateNumOfTailsCombinations(int tossesNum, int tailsNum)
+        {
+            int result = 0;
+            // ...
+
+
+
+            return result;
+        }
+    }
+}
--- Lab1.Source/Program.cs
+++ Lab1.Source/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Lab1.Source.Methods;
+
+namespace Lab1.Source
+{
+    class Program
+    {
+        public static List<int> GetValues(string path)
+        {
+            string text = File.ReadAllText(path);
+            return text.Split(new[] { ' ', ',', }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+        }
+
+        public static void Main(string[] args)
+        {
+            string inputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Files\input.txt");
+            string outputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Files\output.txt");
+
+            List<int> values = GetValues(inputPath);
+            int result = 0;
+
+
+            File.WriteAllText(outputPath, result.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
