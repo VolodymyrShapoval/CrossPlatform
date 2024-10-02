cc1b598d7d53dfcb5f30ea0d90cb03882abcdb4d
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2024-10-03 00:55:25 +0300

    lab2: added calculation method

---
+++ Lab2.Source/Methods/EnergyCalculation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.Source.Methods
+{
+    public class EnergyCalculation
+    {
+        /// <summary>
+        /// Method for calculation of the minimum value of energy to achieve the goal
+        /// </summary>
+        /// <param name="platformsNum">Number of platforms</param>
+        /// <param name="heights">Array of platforms height values</param>
+        /// <returns></returns>
+        public static long CalculateMinEnergyValue(ushort platformsNum, ushort[] heights)
+        {
+            long[] dp = new long[platformsNum];
+            dp[0] = 0;
+            
+            if(platformsNum > 1)
+                dp[1] = heights[1] - heights[0];
+
+            for(int i = 2; i < platformsNum; i++)
+                dp[i] = Math.Min(
+                    dp[i-1] + Math.Abs(heights[i] - heights[i-1]), // neighbours platforms
+                    dp[i - 2] + 3 * Math.Abs(heights[i] - heights[i - 2])); // through one platform
+
+            return dp[platformsNum-1];
+        }
+    }
+}
---
+++ Lab2.Source/Program.cs
@@ -0,0 +1,63 @@
+﻿using Lab2.Source.Methods;
+
+namespace Lab2.Source
+{
+    class Program
+    {
+        public static (ushort, ushort[]) GetValues(string path)
+        {
+            string[] lines;
+
+            if (File.Exists(path))
+                lines = File.ReadAllLines(path).Where(x => x != string.Empty).ToArray();
+            else throw new FileNotFoundException();
+
+            ushort count = 0;
+            ushort[] heights = Array.Empty<ushort>();
+
+            if (lines.Length == 2)
+            {
+                count = ushort.TryParse(lines[0], out ushort result) 
+                    ? result 
+                    : throw new FormatException("The first line has to contain only 1 value of integer type - number of platforms!");
+                heights = lines.Skip(1)
+                    .SelectMany(line => line.Split(new[] { ' ', ',', }, StringSplitOptions.RemoveEmptyEntries))
+                    .Select(x => ushort.TryParse(x, out ushort result) ? result : throw new FormatException("Each value must be an integer and non-negative!"))
+                    .ToArray();
+            }
+            else
+            {
+                throw new InvalidDataException("input.txt has to contain only 2 lines:\n" +
+                    "1. Number of platforms\n" +
+                    "2. High values for each platform");
+            }
+
+            if(count < 1 || count > 30000)
+                throw new ArgumentOutOfRangeException("Number of platforms must be in range 1 <= numberOfPlatforms <= 30000");
+            if(heights.Length < count)
+                throw new ArgumentException($"Number of height values must to equal to the number of platforms({count})");
+            if(heights.All(x => x > 30000))
+                throw new ArgumentOutOfRangeException($"Height values must be in range 0 <= height <= 30000");
+            
+            return (count, heights);
+        }
+        public static void Main(string[] args)
+        {
+            string inputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Files\input.txt");
+            string outputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Files\output.txt");
+
+            try
+            {
+                (ushort count, ushort[] heights) = GetValues(inputPath);
+                long result = EnergyCalculation.CalculateMinEnergyValue(count, heights);
+
+                Console.WriteLine(result);
+                File.WriteAllText(outputPath, result.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
