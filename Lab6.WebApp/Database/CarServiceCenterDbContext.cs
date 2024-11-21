9b238f7b36139cd6dec9ed6100aa043f32166eab
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2024-11-21 20:03:21 +0200

    lab6: added OnModelCreating configurations to dbContext

--- Lab6.WebApp/Database/CarServiceCenterDbContext.cs
+++ Lab6.WebApp/Database/CarServiceCenterDbContext.cs
@@ -1,10 +1,13 @@
-﻿using Lab6.WebApp.Database.Models;
+﻿using Lab6.WebApp.Database.Configurations;
+using Lab6.WebApp.Database.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Lab6.WebApp.Database
 {
-    public class CarServiceCenterDbContext : DbContext
+    public class CarServiceCenterDbContext(DbContextOptions<CarServiceCenterDbContext>) 
+        : DbContext
     {
+
         public DbSet<Manufacturer> Manufacturers { get; set; }
         public DbSet<Model> Models { get; set; }
         public DbSet<Car> Cars { get; set; }
@@ -12,6 +15,19 @@ namespace Lab6.WebApp.Database
         public DbSet<Mechanic> Mechanics { get; set; }
         public DbSet<ServiceBooking> ServiceBookings { get; set; }
         public DbSet<MechanicOnService> MechanicsOnServices { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ManufacturerConfiguration());
+            modelBuilder.ApplyConfiguration(new ModelConfiguration());
+            modelBuilder.ApplyConfiguration(new CarConfiguration());
+            modelBuilder.ApplyConfiguration(new CustomerConfiguration());
+            modelBuilder.ApplyConfiguration(new MechanicConfiguration());
+            modelBuilder.ApplyConfiguration(new ServiceBookingConfiguration());
+            modelBuilder.ApplyConfiguration(new MechanicOnServiceConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
