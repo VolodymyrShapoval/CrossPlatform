2d506d85871823b3c12b16bbda591c4e23f12e2c
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2024-11-22 19:54:35 +0200

    lab6: added manufacturer api controller and connected it to the client program

---
+++ Lab6.WebApp/Controllers/API/ManufacturersDictionaryAPIController.cs
@@ -0,0 +1,42 @@
+﻿using Lab6.WebApp.Database;
+using Lab6.WebApp.Database.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Lab6.WebApp.Controllers.API
+{
+    [ApiController]
+    [Route("api/manufacturers")]
+    public class ManufacturersDictionaryController : ControllerBase
+    {
+        private readonly CarServiceCenterDbContext _dbContext;
+        public ManufacturersDictionaryController(CarServiceCenterDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: api/Manufacturers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Manufacturer>>> Get()
+        {
+            return await _dbContext.Manufacturers
+                .Include(m => m.Models)
+                .ToListAsync();
+        }
+
+        // GET: api/Manufacturers/[id]
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<Manufacturer>> Get(Guid id)
+        {
+            var manufacturer = await _dbContext.Manufacturers
+                .Include(m => m.Models)
+                .FirstOrDefaultAsync(m => m.ManufacturerCode == id);
+
+            return manufacturer == null ? NotFound() : manufacturer;
+        }  
+    }
+}
--- Lab6.WebApp/Controllers/ManufacturersDictionaryController.cs
+++ Lab6.WebApp/Controllers/ManufacturersDictionaryController.cs
@@ -1,22 +1,37 @@
 ﻿using Lab6.WebApp.Database;
+using Lab6.WebApp.Database.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Lab6.WebApp.Controllers
 {
     public class ManufacturersDictionaryController : Controller
     {
-        private readonly CarServiceCenterDbContext _dbContext;
-        public ManufacturersDictionaryController(CarServiceCenterDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
-
         public async Task<IActionResult> Index()
         {
-            var manufacturers = await _dbContext.Manufacturers.ToListAsync();
-            return View(manufacturers);
+            HttpClient httpClient = new HttpClient();
+            try
+            {
+                var httpResponse = await httpClient.GetAsync("http://localhost:3000/api/manufacturers");
+                var jsonData = await httpResponse.Content.ReadAsStringAsync();
+
+                if (jsonData == null) throw new JsonException("There are not any information");
+                var manufacturers = JsonSerializer.Deserialize<List<Manufacturer>>(jsonData, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+
+                return View(manufacturers);
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
